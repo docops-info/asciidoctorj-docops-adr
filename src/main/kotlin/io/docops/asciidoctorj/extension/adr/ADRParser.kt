1644b379c6189740995f33276423ea34d8ebb531
Steve Roach <sroachst@gmail.com>
2022-06-01 23:57:58 -0400

    support multiple urls on single line, urls at beginning of a line as well.

--- src/main/kotlin/io/docops/asciidoctorj/extension/adr/ADRParser.kt
+++ src/main/kotlin/io/docops/asciidoctorj/extension/adr/ADRParser.kt
@@ -20,7 +20,7 @@ package io.docops.asciidoctorj.extension.adr
 import io.docops.asciidoctorj.extension.adr.model.Adr
 import io.docops.asciidoctorj.extension.adr.model.Status
 import java.io.File
-import java.util.StringTokenizer
+import java.util.*
 
 class ADRParser {
 
@@ -78,7 +78,6 @@ class ADRParser {
     }
 
 
-
     private fun mapTitle(map: MutableMap<String, MutableList<String>>): String {
         val title = map["TITLE"]
         require(title != null) { "Invalid syntax title not found" }
@@ -113,7 +112,10 @@ class ADRParser {
         return list
     }
 
-    private fun mapDecision(map: MutableMap<String, MutableList<String>>, config: AdrParserConfig): MutableList<String> {
+    private fun mapDecision(
+        map: MutableMap<String, MutableList<String>>,
+        config: AdrParserConfig
+    ): MutableList<String> {
         val decision = map["DECISION"]
         require(decision != null) { "Invalid syntax decision not found" }
         val list = mutableListOf<String>()
@@ -123,7 +125,10 @@ class ADRParser {
         return list
     }
 
-    private fun mapConsequences(map: MutableMap<String, MutableList<String>>, config: AdrParserConfig): MutableList<String> {
+    private fun mapConsequences(
+        map: MutableMap<String, MutableList<String>>,
+        config: AdrParserConfig
+    ): MutableList<String> {
         val consequences = map["CONSEQUENCES"]
         require(consequences != null) { "Invalid syntax consequences not found" }
         val list = mutableListOf<String>()
@@ -132,7 +137,11 @@ class ADRParser {
         }
         return list
     }
-    private fun mapParticipants(map: MutableMap<String, MutableList<String>>, config: AdrParserConfig): MutableList<String> {
+
+    private fun mapParticipants(
+        map: MutableMap<String, MutableList<String>>,
+        config: AdrParserConfig
+    ): MutableList<String> {
         val parts = map["PARTICIPANTS"]
         val list = mutableListOf<String>()
         parts?.forEach {
@@ -170,26 +179,35 @@ fun String.addLinebreaks(maxLineLength: Int): MutableList<String> {
     return list
 }
 
-fun String.makeUrl(urlMap: MutableMap<Int, String>): String{
-        var key = 0
-        val maxEntry = urlMap.maxByOrNull { it.key }
-        maxEntry?.let {
-            key = it.key
-        }
-
-    if(this.contains("[[") && this.contains("]]")) {
-        val result = this.substringAfter("[[").substringBefore("]]")
-        val sp = result.split(" ")
-        val str = StringBuilder()
-        for(i in 1..sp.size-1) {
-            str.append(sp[i])
-            if(i<sp.size-1) {
-                str.append(" ")
+fun String.makeUrl(urlMap: MutableMap<Int, String>): String {
+    var key = 0
+    val maxEntry = urlMap.maxByOrNull { it.key }
+    maxEntry?.let {
+        key = it.key
+    }
+    var newStr = this
+    if (this.contains("[[") && this.contains("]]")) {
+        val regex = "(?<=\\[\\[)(.*?)(?=]])".toRegex()
+        val matches = regex.findAll(this)
+        val m = mutableMapOf<String, String>()
+        matches.forEach {
+            val sp = it.value.split(" ")
+            val str = StringBuilder()
+            for (i in 1 until sp.size) {
+                str.append(sp[i])
+                if (i < sp.size - 1) {
+                    str.append(" ")
+                }
             }
+            val url = "<tspan><a href=\"${sp[0]}\" xlink:href=\"${sp[0]}\" target=\"_blank\">${str}</a></tspan>"
+            m[url] = it.value
         }
-        val url ="<tspan text-anchor=\"middle\"><a href=\"${sp[0]}\" xlink:href=\"${sp[0]}\" target=\"_blank\">${str}</a></tspan>"
-        urlMap[key+1] = url
-        return this.replace("[[$result]]", "_${key+1}_")
+        var count = key + 1
+        m.forEach { (k, v) ->
+            urlMap[count] = k
+            newStr = newStr.replace("[[${v}]]", "_${count++}_")
+        }
+        return newStr
     }
     return this
 
@@ -206,16 +224,16 @@ fun main() {
          The Data currently resides in RDBMS, it is difficult to expose micro-service directly
          querying out of RDBMS databases since the application runs out of the same environment.
          There are options like Elastic Search and [[https://solr.apache.org/ Solr Rocks]] where data can be replicated. These solutions provide out of the box capabilities
-         that can be leveraged by developers without needed to build RESTful or GraphQL type APIs.
+         that can be leveraged by developers without needed to build [[https://en.wikipedia.org/wiki/Representational_state_transfer RESTful]] or [[https://graphql.org/ GraphQL]] type APIs.
          Decision:Use Solr for data indexing. This use is because Solr has high performance throughput with large volume of data.
          Unstructured data can also be supported.
          If this decision does not meet the need then additional PoC will be created.
-         Consequences:Data in [[https://solr.apache.org/ Solr]] Needs to be replicated across the solr cloud instances.
+         Consequences: [[https://solr.apache.org/ Data]] in [[https://solr.apache.org/ Solr]] Needs to be replicated across the solr cloud instances.
          This Solr cloud needs maintenance.
          Near realtime data replication is required Additional Cost of maintaining the Solr Cloud environment.
         """.trimIndent()
     )
-    var svg =(AdrMaker().makeAdrSvg(adr))
+    var svg = (AdrMaker().makeAdrSvg(adr))
     adr.urlMap.forEach { (t, u) ->
         svg = svg.replace("_${t}_", u)
     }
