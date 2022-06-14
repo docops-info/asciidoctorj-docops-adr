a952c168f18c1de5548a201bd6b6b11fe8ccdd5a
Steve Roach <sroachst@gmail.com>
2022-06-14 19:55:55 -0400

    add popup for adr

--- src/main/kotlin/io/docops/asciidoctorj/extension/adr/AdrSummaryBlockProcessor.kt
+++ src/main/kotlin/io/docops/asciidoctorj/extension/adr/AdrSummaryBlockProcessor.kt
@@ -1,5 +1,7 @@
 package io.docops.asciidoctorj.extension.adr
 
+import io.docops.asciidoctorj.extension.adr.model.Adr
+import org.asciidoctor.ast.Block
 import org.asciidoctor.ast.ContentModel
 import org.asciidoctor.ast.StructuralNode
 import org.asciidoctor.extension.BlockProcessor
@@ -12,6 +14,7 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 import java.time.Duration
+import java.util.*
 
 @Name("adrsummary")
 @Contexts(Contexts.LISTING)
@@ -31,19 +34,122 @@ class AdrSummaryBlockProcessor : BlockProcessor() {
             }
         }
         lines.addAll(closeTable())
-        return createBlock(parent, "open", lines)
+        val argAttributes: MutableMap<String, Any> = HashMap()
+        argAttributes["content_model"] = ":raw"
+        val block: Block = createBlock(parent, "open", "", argAttributes, HashMap<Any, Any>())
+        //language=html
+        val jsFun = """
+            <style>
+            .modal {
+            background: dodgerblue;
+            height: 1px;
+            overflow: hidden;
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            transition: width 0.5s ease 0.5s, height 0.5s ease;
+            width: 0;
+        }
+
+        .adrcontent {
+            color: transparent;
+            font-family: "Consolas", arial, sans-serif;
+            font-size: 2em;
+            position: absolute;
+            top: 50%;
+            left: 10%;
+            text-align: center;
+            transform: translate3d(0, -50%, 0);
+            transition: color 0.5s ease;
+            width: 100%;
+        }
+
+
+
+        input {
+            cursor: pointer;
+            height: 0;
+            opacity: 0;
+            width: 0;
+        }
+        input:focus {
+            outline: none;
+        }
+
+        input:checked {
+            height: 40px;
+            opacity: 1;
+            position: fixed;
+            right: 20px;
+            top: 20px;
+            z-index: 1;
+            -webkit-appearance: none;
+            width: 40px;
+        }
+        input:checked::after, input:checked:before {
+            border-top: 1px solid #FFF;
+            content: "";
+            display: block;
+            position: absolute;
+            top: 50%;
+            transform: rotate(45deg);
+            width: 100%;
+        }
+        input:checked::after {
+            transform: rotate(-45deg);
+        }
+
+        input:checked + label {
+            color: #FFF;
+            transition: color 0.5s ease;
+        }
+
+        input:checked ~ .modal {
+            height: 100%;
+            width: 100%;
+            transition: width 0.5s ease, height 0.5s ease 0.5s;
+        }
+        input:checked ~ .modal .adrcontent {
+            color: #FFF;
+            transition: color 0.5s ease 0.5s;
+        }
+            </style>
+            <script>
+            function hideSVG(id) {
+              var style = document.getElementById(id).style.display;
+              if(style === "none")
+                document.getElementById(id).style.display = "block";
+              else
+                document.getElementById(id).style.display = "none";
+              //or to hide the all svg
+              //document.getElementById("mySvg").style.display = "none";
+            }
+            </script>
+        """.trimIndent()
+        block.blocks.add(createBlock(parent, "pass","""
+    $jsFun
+        """.trimIndent()))
+
+        val tblBlock = (createBlock(parent, "open", lines))
+        parseContent(block, tblBlock.lines)
+        return block
     }
 
     private fun adrFromFile(f: File): MutableList<String> {
-        val adr = ADRParser().parse(f.readText(), AdrParserConfig())
         val lines = mutableListOf<String>()
-        lines.add("a|${adr.title} |${adr.status} |${adr.participantAsStr()} |${adr.date}")
+
+        val adr = ADRParser().parse(f.readText(), AdrParserConfig())
+        val div = buildDiv(adr)
+        lines.add("a|${adr.title} |  +++ $div +++ |${adr.participantAsStr()} |${adr.date}")
+
+        //lines.add("a|${adr.title} |${adr.status} |${adr.participantAsStr()} |${adr.date}")
         return lines
     }
 
     private fun startTable(): MutableList<String> {
         val lines = mutableListOf<String>()
-        lines.add("[options=header]")
+        lines.add("[options=header,cols=\"1,1,1,1\"]")
         lines.add("|===")
         //title, status, participants and date
         lines.add("|Title |Status |Participants |Date")
@@ -70,7 +176,8 @@ class AdrSummaryBlockProcessor : BlockProcessor() {
             val response = client.send(request, HttpResponse.BodyHandlers.ofString())
             if (200 == response.statusCode()) {
                 val adr = ADRParser().parse(response.body(), AdrParserConfig())
-                lines.add("a|${adr.title} |${adr.status} |${adr.participantAsStr()} |${adr.date}")
+                val div = buildDiv(adr)
+                lines.add("a|${adr.title} |  +++ $div +++ |${adr.participantAsStr()} |${adr.date}")
             } else {
                 lines.add("4+|$url does not exist.")
             }
@@ -79,5 +186,23 @@ class AdrSummaryBlockProcessor : BlockProcessor() {
         }
 
         return lines
+    }
+    private fun buildDiv(adr: Adr): String {
+        var svg = (AdrMaker().makeAdrSvg(adr))
+        adr.urlMap.forEach { (t, u) ->
+            svg = svg.replace("_${t}_", u)
+        }
+        val str = Base64.getEncoder().encodeToString(svg.toByteArray())
+        val now = System.currentTimeMillis()
+        val imageStr = """<img src="data:image/svg+xml;base64,$str"></img>"""
+        return """
+                    <div id="mod$now">
+                        <input id='button$now' type='checkbox'>
+                        <label for='button$now'>${adr.status}</label>
+                        <div class='modal'>
+                            <div class='adrcontent'>$imageStr</div>
+                        </div>
+                    </div>
+                """.trimIndent()
     }
 }
